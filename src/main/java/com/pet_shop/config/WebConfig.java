61331adaba42ec78ebb6234b640cf72584c790cf
Dawid <prawica.dawid@gmail.com>
2018-04-30 18:15:47 +0200

    view resolver

--- src/main/java/com/pet_shop/config/WebConfig.java
+++ src/main/java/com/pet_shop/config/WebConfig.java
@@ -1,4 +1,24 @@
 package com.pet_shop.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan({"com.pet_shop"})
+public class WebConfig extends WebMvcConfigurerAdapter{
+
+    @Bean
+    public InternalResourceViewResolver getView(){
+        InternalResourceViewResolver view = new InternalResourceViewResolver();
+        view.setViewClass(JstlView.class);
+        view.setPrefix("/WEB-INF/views/");
+        view.setSuffix(".jsp");
+        return view;
+    }
 }
