9753aa5e1db8906879e0ffe4f49521b212ae9276
Dawid <prawica.dawid@gmail.com>
2018-05-01 12:25:29 +0200

    hiberanate setup

--- src/main/java/com/pet_shop/config/DataBaseConfig.java
+++ src/main/java/com/pet_shop/config/DataBaseConfig.java
@@ -1,9 +1,45 @@
 package com.pet_shop.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @EnableTransactionManagement
+@ComponentScan({"com.pet_shop"})
 public class DataBaseConfig {
+
+    @Bean
+    public DataSource getDataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/petstore?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
+        dataSource.setUsername("root");
+        dataSource.setPassword("123456");
+        return dataSource;
+    }
+
+    @Bean
+    public Properties getHibernateProperties(){
+        Properties properties = new Properties();
+        properties.put("hibernate.hbm2ddl.auto", "update");
+        properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+        properties.put("hibernate.show_sql", "true");
+        return properties;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean getSessionFactory(){
+        LocalSessionFactoryBean localSession = new LocalSessionFactoryBean();
+        localSession.setPackagesToScan(new String[] {"com.pet_shop"});
+        localSession.setDataSource(getDataSource());
+        localSession.setHibernateProperties(getHibernateProperties());
+        return localSession;
+    }
 }
