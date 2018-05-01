2442dd1eb6345adb7ee735cbfc7ed542ac212fd6
Dawid <prawica.dawid@gmail.com>
2018-05-01 10:49:02 +0200

    begin hibernate configuration

---
+++ src/main/java/com/pet_shop/config/DataBaseConfig.java
@@ -0,0 +1,9 @@
+package com.pet_shop.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class DataBaseConfig {
+}
