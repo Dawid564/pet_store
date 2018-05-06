5ef18442286b5fef9262119b1f5fa2d4da85287e
Dawid <prawica.dawid@gmail.com>
2018-05-06 21:09:42 +0200

    prepeare rest controller

--- src/main/java/com/pet_shop/controller/RestController.java
+++ src/main/java/com/pet_shop/controller/RestController.java
@@ -1,11 +1,29 @@
 package com.pet_shop.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.pet_shop.domain.Pet;
+import org.springframework.web.bind.annotation.*;
 
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping("/operations")
 public class RestController {
 
+    @GetMapping("/{animalId}")
+    public String getAnimal(@PathVariable String animalId){
+        return null;
+    }
 
+    @PostMapping("/addAnimal")
+    public String addAnimal(@ModelAttribute Pet pet){
+        return null;
+    }
 
+    @PutMapping("/updateAnimal")
+    public String updateAnimal(@ModelAttribute Pet pet){
+        return null;
+    }
+
+    @DeleteMapping("/deleteAnimal/{animalId}")
+    public String deleteAnimal(@PathVariable String animalId){
+        return null;
+    }
 }
